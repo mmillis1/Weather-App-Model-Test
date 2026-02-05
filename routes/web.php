e26f64da4de76429fcee4376fb0f1ad936814141
Michael Millis <mmillis1@gmail.com>
2026-02-05 07:45:19 -0800

    kimi k2-5 one shot

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WeatherController::class, 'index'])->name('home');
+
+Route::post('/api/weather', [WeatherController::class, 'getWeather'])
+    ->middleware('throttle:30,1')
+    ->name('weather.get');
